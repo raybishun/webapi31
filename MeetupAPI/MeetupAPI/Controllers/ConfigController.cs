7c65400537aff421986bbca6de95a8aeb445b331
RayBishun <ray.bishun@bitsbytes.com>
2022-10-31 19:36:41 -0400

    Implemented a Function Action

---
+++ MeetupAPI/MeetupAPI/Controllers/ConfigController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetupAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpOptions("reload")]
+        public ActionResult Reload()
+        {
+            try
+            {
+                ((IConfigurationRoot)_configuration).Reload();
+                
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
